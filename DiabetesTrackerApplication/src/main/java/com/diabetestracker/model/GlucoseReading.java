d8db230d78ea4028cd926f0324cb1091dbfd00d9
Noureddine Bouzire <bouzire.noureddine1@gmail.com>
2024-06-06 09:50:22 +0100

    model

--- DiabetesTrackerApplication/src/main/java/com/diabetestracker/model/GlucoseReading.java
+++ DiabetesTrackerApplication/src/main/java/com/diabetestracker/model/GlucoseReading.java
@@ -1,2 +1,45 @@
-package com.diabetestracker.model;public class GlucoseReading {
+package com.diabetestracker.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "glucose_reading")
+public class GlucoseReading {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "date_time")
+    private LocalDateTime dateTime;
+
+    @Column(name = "glucose_level")
+    private int glucoseLevel;
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public int getGlucoseLevel() {
+        return glucoseLevel;
+    }
+
+    public void setGlucoseLevel(int glucoseLevel) {
+        this.glucoseLevel = glucoseLevel;
+    }
 }
